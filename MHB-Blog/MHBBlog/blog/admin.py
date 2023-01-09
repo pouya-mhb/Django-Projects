a4db040ad88ce7016021563f4e93679e2f812f06
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-01-10 03:00:48 +0330

    create admin model and customide the admin panel

--- MHB-Blog/MHBBlog/blog/admin.py
+++ MHB-Blog/MHBBlog/blog/admin.py
@@ -3,4 +3,30 @@ from blog.models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'author' , 'publish_date' , 'status' , 'slug'
+    ]
+
+    list_filter = [
+        'title' , 'status' , 'publish_date'  , 'created_date'
+    ]
+
+    search_fields = [
+        'title' , 'status' , 'publish_date'  , 'created_date'
+    ]
+
+    prepopulated_fields = {
+        'slug' : ('title','author') # i love this one
+    }
+
+    raw_id_fields = ('author',)
+
+    date_hierarchy = 'publish_date'
+
+    ordering = (
+        'author' , 'publish_date' , 'status'
+    )
