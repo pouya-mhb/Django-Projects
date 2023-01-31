e5320293877f0f81d9599ff1fe67a00e818147ae
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-02-01 00:45:52 +0330

    add post list view and url

--- MHB-Blog/MHBBlog/MHBBlog/urls.py
+++ MHB-Blog/MHBBlog/MHBBlog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/',views.post_list,name='posts')
 ]
--- MHB-Blog/MHBBlog/blog/views.py
+++ MHB-Blog/MHBBlog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+
+def post_list (request):
+    posts = Post.objects.all()
+    context = {
+            'posts':posts
+        }
+    return render(
+        request,'PostList.html',context
+    )
